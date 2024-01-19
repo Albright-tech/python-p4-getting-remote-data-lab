b9f6c8462b72150c60b69423c5a11f61a26d4434
Albright <albright.muchori@student.moringaschool.com>
2024-01-19 12:14:39 +0300

    getting remote data

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,12 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        parsed_response = json.loads(self.get_response_body())
+        return parsed_response
+
+get_requester = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+print(get_requester.load_json())
